f7ac97765a96106fcc3a51d250b8442c69ca02e8
Md. Nazmul Hasan <nazmul.hasan@technovista.com.bd>
2020-12-14 19:04:13 +0600

    Added vue form

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -9,21 +9,32 @@
             <div class="row justify-content-center">
                 <div class="col-md-4">
                     <div class="card shadow">
+                      <vue-progress-bar></vue-progress-bar>
                         <div class="card-body">
                             <header>
                                 <h2 class="text-uppercase text-center h5 mb-4 text-secondary font-weight-bold">Login</h2>
                             </header>
                             <hr>
-                            <form method="POST" action="#">
+                            <vue-form :state="formstate" @submit.prevent="onSubmit">
+                              <validate tag="login">
                                 <div class="form-group">
                                     <label class="text-sm-left" for="login">Email</label>
-                                    <input id="login" type="text" class="form-control" name="login" value="" required autofocus>
+                                    <input v-model="model.email" id="login" type="email" class="form-control" name="email" value="" required autofocus>
+                                    <field-messages name="email" show="$dirty && $touched">
+                                        <div class="text-danger" slot="required">Email is a required field</div>
+                                        <div class="text-danger" slot="email">Email is not valid</div>
+                                    </field-messages>
                                 </div>
-
+                              </validate>
+                              <validate tag="password">
                                 <div class="form-group">
                                     <label for="password">Password</label>
-                                    <input id="password" type="password" class="form-control" name="password" required autocomplete="current-password">
+                                    <input v-model="model.password"  id="password" type="password" class="form-control" name="password" required autocomplete="current-password">
+                                     <field-messages name="password" show="$dirty && $touched">
+                                        <div class="text-danger" slot="required">Password is a required field</div>
+                                    </field-messages>
                                 </div>
+                              </validate>
                                 <div class="form-group row mb-0">
                                     <div class="col text-sm-right">
                                         <button type="submit" class="btn btn-primary">
@@ -32,7 +43,7 @@
                                     </div>
                                 </div>
                                 <hr>
-                            </form>
+                            </vue-form>
                         </div>
                         <!-- /.card-body -->
                     </div>
@@ -49,13 +60,50 @@
 </template>
 
 <script>
+import VueForm from 'vue-form'
 export default {
   name: 'Login',
   data () {
     return {
-      msg: ''
+      formstate: {},
+      model: {
+        password: '',
+        email: ''
+      },
+      options: {
+        color: '#007AFF',
+        strokeWidth: 0.5,
+        duration: 1200,
+      }
     }
+  },
+  methods: {
+  onSubmit: function () {
+    if (this.formstate.$invalid) {
+      // alert user and exit early
+      //this.$refs.line.animate(1.0)
+      this.$Progress.start()
+      // return
+    } else {
+      /* axios.get('https://api.example.com/', {
+            params: {
+              page: this.page++,
+              per_page: this.pageSize,
+            }
+          })
+          .then(res => {
+            this.images.concat(res.data)
+            
+            // Stop scroll-loader
+            res.data.length < this.pageSize && (this.loadMore = false)
+          })
+          .catch(error => {
+            console.log(error);
+          }) */
+    }
+    
   }
+}
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -6,9 +6,30 @@ import App from './App'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import VueForm from 'vue-form'
+import VueProgressBar from 'vue-progressbar'
 Vue.config.productionTip = false
 
+const options = {
+  color: '#bffaf3',
+  failedColor: '#874b4b',
+  thickness: '5px',
+  transition: {
+    speed: '0.2s',
+    opacity: '0.6s',
+    termination: 300
+  },
+  autoRevert: true,
+  location: 'left',
+  inverse: false
+}
+ 
+Vue.use(VueProgressBar, options)
 Vue.use(BootstrapVue)
+Vue.use(VueForm)
+
+
+const APIURL = "https://api.chucknorris.io/jokes";
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
