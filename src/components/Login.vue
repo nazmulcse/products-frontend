586eaf2172c3a7d095e9ebf4397eca71e26d72cd
Md. Nazmul Hasan <nazmul.hasan@technovista.com.bd>
2020-12-14 15:55:40 +0600

    Added login page

---
+++ src/components/Login.vue
@@ -0,0 +1,78 @@
+<template>
+<div class="d-flex h-100 align-items-center">
+    <div class="w-100">
+        <div class="text-center">
+            <h1 class="font-weight-bold h3"><a href="https://mislgsp.gov.bd/bn/login">Product Store</a></h1>
+        </div>
+        <main class="main">
+          <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-4">
+                    <div class="card shadow">
+                        <div class="card-body">
+                            <header>
+                                <h2 class="text-uppercase text-center h5 mb-4 text-secondary font-weight-bold">Login</h2>
+                            </header>
+                            <hr>
+                            <form method="POST" action="#">
+                                <div class="form-group">
+                                    <label class="text-sm-left" for="login">Email</label>
+                                    <input id="login" type="text" class="form-control" name="login" value="" required autofocus>
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="password">Password</label>
+                                    <input id="password" type="password" class="form-control" name="password" required autocomplete="current-password">
+                                </div>
+                                <div class="form-group row mb-0">
+                                    <div class="col text-sm-right">
+                                        <button type="submit" class="btn btn-primary">
+                                            Login
+                                        </button>
+                                    </div>
+                                </div>
+                                <hr>
+                            </form>
+                        </div>
+                        <!-- /.card-body -->
+                    </div>
+                    <!-- /.card -->
+                    <footer class="text-center text-muted small mt-2">
+                        Design & Developed By <a href="#" class="text-reset" target="_blank" rel="noopener">Nazmul Hasan</a>
+                    </footer>
+                </div>
+            </div>
+          </div>
+      </main>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      msg: ''
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
